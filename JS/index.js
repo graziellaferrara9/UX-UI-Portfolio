825f7ae11a34bf6a7fcb8f4028c89136d6447252
graziellaferrara9 <graziellaferrara9@gmail.com>
2021-10-23 18:13:44 -0400

    Updates 10.23.2021

---
+++ JS/index.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+$("#aboutMe").mouseover(function(){
+    $("#aboutMe").css("background-color","#A49586");
+});
+$("#aboutMe").mouseleave(function(){
+    $("#aboutMe").css("background-color","#EBE3CF");
+});
+$("#work").mouseover(function(){
+    $("#work").css("background-color","#A49586");
+});
+$("#work").mouseleave(function(){
+    $("#work").css("background-color","#EBE3CF");
+});
+
+$("#contact").mouseover(function(){
+    $("#contact").css("background-color","#A49586");
+});
+$("#contact").mouseleave(function(){
+    $("#contact").css("background-color","#EBE3CF");
+});
+
+$("#resume").mouseover(function(){
+    $("#resume").css("background-color","#A49586");
+});
+$("#resume").mouseleave(function(){
+    $("#resume").css("background-color","#EBE3CF");
+});
+
+});
+
